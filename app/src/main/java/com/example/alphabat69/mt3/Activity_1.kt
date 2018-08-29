2f0db975511abd83d2ff6f36e548b78eb78013f2
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2018-08-29 19:36:33 +0530

    initial commit

---
+++ app/src/main/java/com/example/alphabat69/mt3/Activity_1.kt
@@ -0,0 +1,13 @@
+package com.example.alphabat69.mt3
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class Activity_1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_1)
+    }
+}
